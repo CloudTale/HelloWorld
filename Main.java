d0f19457adc6ab7a1316dbf5249f67b77c8396a9
CloudTale <69694439+CloudTale@users.noreply.github.com>
2020-08-14 13:05:58 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+      System.out.println("My name is Kevin");
+      System.out.println("I am 16 years old");
+      System.out.println("My hobbies are soccer and music");
+      System.out.println("I am interested in learning how to code");
+      System.out.println("I can already code but I want to learn more");
+   }
+}
